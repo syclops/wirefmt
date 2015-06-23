65e177effccc8b4a0066f1ea007ed5605c74d0b7
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-24 00:09:25 +0200

    Added argument parsing for format width

--- wirefmt.py
+++ wirefmt.py
@@ -2,8 +2,14 @@
 # Author: Steve Matsumoto
 # Copyright 2015
 
+import argparse
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--width', type=int, choices=[16, 32],
+                        default=32,
+                        help='width of the format in bits (default: 32)')
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
