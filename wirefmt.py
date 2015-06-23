cd79d691e2aa098786b6c8dcfb55aef5d6f1e60e
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-24 00:18:25 +0200

    Printing the bit numbers

--- wirefmt.py
+++ wirefmt.py
@@ -4,12 +4,17 @@
 
 import argparse
 
+def printNums(width):
+    print ''.join([' ' + (' ' if i < 10 else str(i/10)) for i in range(width)])
+    print ''.join([' ' + str(i % 10) for i in range(width)])
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', '--width', type=int, choices=[16, 32],
                         default=32,
                         help='width of the format in bits (default: 32)')
     args = parser.parse_args()
+    printNums(args.width)
 
 if __name__ == '__main__':
     main()
