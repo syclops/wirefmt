0a77288fcfcdbbbf31e9f9761c0e26c6b72da5e8
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-24 18:22:52 +0200

    Added functions to print the bit numbers as well as the header and footer lines

--- printer.py
+++ printer.py
@@ -11,6 +11,17 @@ class Printer(object):
         self.width = width
 
     def __repr__(self):
-        out_str = ''
+        return '\n'.join([self._bit_numbers(),
+                          self._header_footer_line(),
+                          self._header_footer_line()])
+
+    def _bit_numbers(self):
+        out_str = ' '
+        out_str += ' '.join(
+            [' ' if i < 10 else str(i/10) for i in range(self.width)])
+        out_str += '\n '
+        out_str += ' '.join([str(i%10) for i in range(self.width)])
         return out_str
 
+    def _header_footer_line(self):
+        return '-'.join('+' * (self.width + 1))
