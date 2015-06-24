515471549ae969246722e4a8f41de03e0c37c318
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-24 18:30:04 +0200

    Added support for inputting specification files

--- wirefmt.py
+++ wirefmt.py
@@ -3,6 +3,7 @@
 # Copyright 2015
 
 import argparse
+import fileinput
 
 from printer import Printer
 
@@ -11,8 +12,12 @@ def main():
     parser.add_argument('-w', '--width', type=int, choices=[16, 32],
                         default=32,
                         help='width of the format in bits (default: 32)')
+    parser.add_argument('file', help='Path of field specification file')
     args = parser.parse_args()
     p = Printer(args.width)
+    for line in fileinput.input(args.file):
+        [name, length] = line.strip().split()
+        p.add_field(name, length)
     print p
 
 if __name__ == '__main__':
