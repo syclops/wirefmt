a1e9efdb0b08836b70a4f920aa01cabb79ddf159
Steve Matsumoto <steve@stevematsumoto.net>
2015-06-24 18:24:25 +0200

    Added print function to display the representation in wirefmt.py

--- wirefmt.py
+++ wirefmt.py
@@ -13,6 +13,7 @@ def main():
                         help='width of the format in bits (default: 32)')
     args = parser.parse_args()
     p = Printer(args.width)
+    print p
 
 if __name__ == '__main__':
     main()
